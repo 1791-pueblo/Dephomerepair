aea80669be6c28a2d06a5de0ff2cf93790a2494d
1791-pueblo <nosajpalmer@gmail.com>
2026-09-07 22:51:34 -0700

    Restore homepage and wire portfolio data + copy

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,18 @@
-PLACEHOLDER
+'use client';
+
+import { useEffect, useMemo, useState } from 'react';
+import Image from 'next/image';
+import { ProjectPhotoSlider } from '../components/ProjectPhotoSlider';
+import { portfolioProjects } from '../lib/portfolio';
+import {
+  allServices,
+  applyBundleDiscount,
+  deviceSellPrice,
+  drywall,
+  electrical,
+  lineTotal,
+  plumbing,
+  SERVICE_CALL,
+  serviceCallAmount,
+  type ServicePrice,
+} from '../lib/pricing';
