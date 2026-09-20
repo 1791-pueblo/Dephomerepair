296fdbdd438847adf40fc22d85acb4417cc15d9a
1791-pueblo <nosajpalmer@gmail.com>
2026-09-20 04:20:13 -0700

    Restore homepage and apply brand colors plus About section

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,6 @@
-PLACEHOLDER
+'use client';
+
+import { useEffect, useMemo, useState } from 'react';
+import Image from 'next/image';
+import { ProjectPhotoSlider } from '../components/ProjectPhotoSlider';
+import { portfolioProjects, tradeCardProjects } from '../lib/portfolio';
